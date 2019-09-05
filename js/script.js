0a1ce237ab2d3bfdafe9d4e527db243380db9443
shyam13 <myk2030@gmail.com>
2019-09-05 15:25:33 +0300

    more changes
    more changes

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    var username = document.getElementById("username").value;
+    if (username == "") {
+        document.getElementById("helpUser").innerHTML="INVALID STATE";
+    }
+}
